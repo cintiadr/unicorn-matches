47814e4c7ee5a7831f6e3bfcf24d9f45e27405ba
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2020-10-14 22:20:19 +1100

    Adding multiple genders for a person

--- matches.py
+++ matches.py
@@ -40,7 +40,7 @@ class Person:
     def __init__(self, name, email, identifies_as, searching_for, extra_dates):
         self.name = name.strip()
         self.email = email.strip()
-        self.identifies_as = identifies_as.strip()
+        self.identifies_as = identifies_as.strip().split('|')
         self.searching_for = searching_for.strip().split('|')
         self.extra_dates = extra_dates.strip()
         self.matches = []
@@ -48,12 +48,13 @@ class Person:
 
     def matches_with(self, possible_match):
         print("Attempting match: %s and %s" % (self.email, possible_match.email))
-        if self.identifies_as in possible_match.searching_for and possible_match.identifies_as in self.searching_for:
-            print('Yep')
-            return True
-        else:
-            print('Nope')
-            return False
+        for i in self.identifies_as:
+            for j in possible_match.identifies_as:
+                if i in possible_match.searching_for and j in self.searching_for:
+                    print('Yep')
+                    return True
+        print('Nope')
+        return False
 
 class Date:
     def __init__(self, person1, person2):
